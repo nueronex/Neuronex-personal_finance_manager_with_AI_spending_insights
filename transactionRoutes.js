eb50daf00c6804061e01925c7b088233f5f6d9b6
NeuroNex <neuronex493@gmail.com>
2025-09-13 16:46:31 +0530

    Delete transactionRoutes.js

--- transactionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { addTransaction, getTransactions } = require("../controllers/transactionController");
-const auth = require("../middleware/auth");
-const router = express.Router();
-
-router.post("/", auth, addTransaction);
-router.get("/", auth, getTransactions);
-
-module.exports = router;
